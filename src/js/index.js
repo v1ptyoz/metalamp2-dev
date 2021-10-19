54f29a8560134b5ea1509906128c20d5847782d8
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-10-19 21:03:41 +1000

    refactor: cleanup code

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,2 @@
 import 'normalize.css';
-import MontserratBoldSvg from '../assets/fonts/Montserrat-Bold.svg'
-import MontserratBoldWoff from '../assets/fonts/Montserrat-Bold.ttf'
-import MontserratBoldTtf from '../assets/fonts/Montserrat-Bold.woff'
-import MontserratSvg from '../assets/fonts/Montserrat-Regular.svg'
-import MontserratWoff from '../assets/fonts/Montserrat-Regular.ttf'
-import MontserratTtf from '../assets/fonts/Montserrat-Regular.woff'
-
 import '../sass/style.scss';
